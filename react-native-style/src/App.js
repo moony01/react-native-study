3a51aedb0aa71dfaea7c95d40ec37802f091b357
mhhan <mun01180@gmail.com>
2022-11-16 10:59:24 +0900

    1

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { viewStyles, textStyles } from './styles';
 import { Header, Contents, Footer } from './components/Layout';
+import { Box } from './components/ShadowBox';
 
 const App = () => {
   return (
     <View style={viewStyles.container}>
-      <Header />
-      <Contents />
-      <Footer />
+      <Box />
     </View>
 
   );
